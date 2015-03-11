65a160447a2a032df6e587934ebd82d45cf1c537
Emre Surmeli <s.emre.s.8@gmail.com>
2015-03-11 14:04:56 -0700

    comments added

--- ch02_arrays/grades_object.js
+++ ch02_arrays/grades_object.js
@@ -2,16 +2,21 @@
 // object. Provide a function for adding and a function for displaying the 
 // student's grade average.
 
+
+/* Create Grades class */
 function Grades () {
   this.grades = [];
   this.addGrade = addGrade;
   this.average = average;
 }
 
+/* Create addGrade function that adds grades data to an array */
 function addGrade (grade) {
   this.grades.push(grade);
 }
 
+/* Create an average function that calculates and returns the 
+average of the grades in an array */
 function average () {
   var total = 0;
   for(var i = 0; i < this.grades.length; ++i) {
