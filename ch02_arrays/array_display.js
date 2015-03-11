1826e5c5ef3a587acdb98a2afe031a1089deb857
Emre Surmeli <s.emre.s.8@gmail.com>
2015-03-11 14:15:48 -0700

    comments added

--- ch02_arrays/array_display.js
+++ ch02_arrays/array_display.js
@@ -1,20 +1,28 @@
 // Store a set of words in an array and display the contents both forward
 // and backward. 
 
+/* create empty array */
 var array = [];
 
+/* create a function to add string to the array object */
 function addString (string) {
   this.array.push(string);
 }
 
+/* create a function to display the contents of the array forward
+   using the .toString() method */
 function displayForwardOne (array) {
   return array.toString();
 }
 
+/* create a funciton to display the contents of the array forward
+   using the .join() method */
 function displayForwardTwo (array) {
   return array.join();
 }
 
+/* create a function to display the contents of the array backwards
+   using the .reverse() method */
 function displayBackward (array) {
   return array.reverse();
 }
