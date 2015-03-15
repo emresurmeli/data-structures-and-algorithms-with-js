d6043e327bfc99fd029d968d2167d790c0a3d01a
Emre Surmeli <s.emre.s.8@gmail.com>
2015-03-14 19:41:39 -0700

    fix comments

--- ch04_stacks/exercises.js
+++ ch04_stacks/exercises.js
@@ -8,31 +8,21 @@ function Stack () {
   this.peek = peek;
 }
 
-/* Create a push function to add elements to the Stack */
-
 function push (element) {
   this.dataStore[this.top++] = element;
 }
-
-/* Create a pop function to remove elements from the Stack */
 
 function pop () {
   return this.dataStore[--this.top];
 }
 
-/* Create a peek function to take a look at what is on top of the Stack */
-
 function peek () {
   return this.dataStore[this.top -1];
 }
 
-/* Create a length function to check... you know, the length of the Stack */
-
 function length () {
   return this.top;
 }
-
-/* Create a clear function to erase all the elements in the Stack */
 
 function clear () {
   this.top = 0;
